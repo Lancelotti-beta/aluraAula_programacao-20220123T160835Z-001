d9c9012f43012a2b88c1aef8621c99b027492636
Bruno <brunojosemarques.pe@gmail.com>
2022-03-04 22:02:12 -0300

    Lógica de Programação - Part VIII. Aprendendo enviar do Js para o HTML.

--- aluraAula_programacao/adivinha-game/adivinha_numero.js
+++ aluraAula_programacao/adivinha-game/adivinha_numero.js
@@ -1,8 +1,44 @@
-var numeroDaSorte = 5;
+
+
+//
+// function pulaLinha(vezes){
+//   cont = 1;
+//   while (cont <= vezes) {
+//     document.write("<br>");
+//     //document.write("<hr>");
+//     cont++;
+//   }
+//
+// }
+//
+// function mostraTexto(frase){
+//   document.write("<big>" + frase + "</big>");
+//   pulaLinha(1);
+//
+// }
+
+
+var numeroDaSorte = sorte(10);
 
 var campoDeBusca = document.querySelector("input");
-
 var botao = document.querySelector("button");
+
+// var texto = document.querySelector("p");
+//
+// function maiorMenorDica(){
+//   if(campoDeBusca.value > numeroDaSorte){
+//     texto.value = "O numero mágico era menor que " + campoDeBusca.value;
+//   } else if(campoDeBusca.value< numeroDaSorte) {
+//     texto.value = "O número mágico era maior que " + campoDeBusca.value;
+//   } else {
+//     texto.value = "Parabéns, você acertou o nomero Mágico!";
+//   }
+// }
+// Tentando testar uma mensagem na tela. ainda não esta dendo certo.
+
+function sorte(n){
+  return Math.round(Math.random()*n);
+}
 
 function comparaNumeroDaSorte(){
   if(campoDeBusca.value == numeroDaSorte){
@@ -10,6 +46,9 @@ function comparaNumeroDaSorte(){
   } else {
     alert(" Você Errou. :/  ");
   }
+
+  campoDeBusca.value = "";
+  campoDeBusca.focus();
 
 }
 
