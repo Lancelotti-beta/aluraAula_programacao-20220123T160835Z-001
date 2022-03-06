1af94cb4cdf074f6b297376c4a604ee90dd38b02
Bruno <brunojosemarques.pe@gmail.com>
2022-03-06 20:48:49 -0300

    reorganização dos arquivos. Iniciando Logica de Programação II-Parte I

---
+++ aluraAula_programacao/logica-programacaoII/art.js
@@ -0,0 +1,33 @@
+
+var quadro = document.querySelector('canvas');
+var pincel = quadro.getContext('2d');
+
+function pintaQuadro(color, x, y, largura, altura){
+  pincel.fillStyle = color;
+  pincel.fillRect(x, y, largura, altura);
+}
+
+function corPincel(color){
+  pincel.fillStyle = color;
+}
+
+/*  cor Quadro */
+pintaQuadro("white", 0, 0, 705, 305);
+
+/* trabalhando com cores */
+pintaQuadro("green", 0, 0, 200, 305);
+
+pintaQuadro("red", 505, 0, 200, 305);
+
+/* Criando formas */
+corPincel("yellow");
+pincel.beginPath();
+pincel.moveTo(0, 152.5);
+pincel.lineTo(200,0);
+pincel.lineTo(200, 305);
+pincel.fill();
+
+corPincel("blue");
+pincel.beginPath();
+pincel.arg(400, 152.5, 75, 0, 2 * 3.14);
+pincel.fill();
